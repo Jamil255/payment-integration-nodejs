e11cb9822ecdf2e48bc1b20a6c176048d8d6c7d5
unknown <jamilafzal255@gmail.com>
2024-06-19 18:11:40 +0500

    stripe integration in client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,41 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
-
+import StripeCheckout from 'react-stripe-checkout'
 function App() {
-  const [count, setCount] = useState(0)
+  const [product, setproduct] = useState({
+    name: 'react ',
+    price: 10,
+    productBy: 'facebook',
+  })
 
+  const makePayment = async (token) => {
+    const obj = {
+      token,
+      product,
+    }
+    const payload = await axios.post('http://localhost:3000/payment', obj)
+    console.log(payload)
+  }
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <StripeCheckout
+          stripeKey={import.meta.env.VITE_PUBLISH_API}
+          token={makePayment}
+          name="Buy React"
+          amount={product.price * 100}
+          shippingAddress
+          billingAddress
+        >
+          <button>Buy React in {product.price}$</button>
+        </StripeCheckout>
       </p>
     </>
   )
